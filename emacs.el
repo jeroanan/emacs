6fa333be38b036ca01404437ef8315523fb96b9a
David Wilson <davidwil@posteo.de>
2020-10-11 17:17:16 +0100

    updates to emacs config

--- emacs.el
+++ emacs.el
@@ -4,7 +4,6 @@
 (global-set-key [f10] 'save-buffers-kill-emacs)
 (global-set-key [f11] 'revert-buffer)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-unset-key "\C-x\C-c")
 (global-unset-key "\C-z")
 (global-unset-key "\C-x\C-z")
 (global-unset-key "\C-xm")
@@ -17,17 +16,26 @@
 (global-set-key "\C-x\C-b" 'switch-to-buffer)
 (global-set-key "\C-x\C-p" 'emms-pause)
 
+(defun auto-complete-mode-maybe ()
+  ""
+  (unless (minibufferp (current-buffer))
+    (auto-complete-mode t)))
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+(setq ac-modes '(racket-mode))
+
 (server-start)
 
-;; Get rid of unneeded screen chrome.
-(defun turn-off-screen-crap ()
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1))
+(setq viper-mode t)
+(require 'viper)
 
-(case window-system
-  (x (turn-off-screen-crap))
-  (otherwise ()))
+(setq inhibit-startup-message t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 (setq make-backup-files nil)
 (column-number-mode)
@@ -44,7 +52,9 @@
 (require 'package)
 
 ; The contents of package-list will be downloaded and installed on startup if they're not already present.
-(setq package-list '(projectile auto-complete jedi magit))
+(setq package-list '(projectile auto-complete jedi solarized-theme magit racket-mode linum-relative))
+
+(linum-relative-global-mode)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
@@ -67,26 +77,11 @@
 (setq default-tab-width 3)
 (setq indent-tabs-mode -1)
 (global-linum-mode 1) ;; Line numbers in all buffers
+
 (setq ring-bell-function 'ignore) ;; turn off the bell
 (setq js-indent-level 2)
 (add-hook 'racket-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'elisp-mode-hook 'rainbow-delimiters-mode)
-
-;; Python IDE -- https://www.youtube.com/watch?v=6BlTGPsjGJk
-(require 'projectile)
-(projectile-global-mode)
-
-(require 'auto-complete-config)
-(ac-config-default)
-(setq ac-show-menu-immediately-on-auto-complete t)
-
-(require 'jedi)
-(setq jedi:server-args
-		'("--sys-path" "/home/david/src/GameCollection"))
-
-(defvar jedi-config:vcs-root-sentinel ".git")
-(defvar jedi-config:python-module-sentinel "__init__.py")
-(add-hook 'python-mode-hook 'jedi:setup)
 
 (random t)
 
@@ -111,8 +106,6 @@ WARNING: this is a simple implementation. The chance of generating the same UUID
   (setq buffer-display-table (make-display-table))
   (aset buffer-display-table ?\^M []))
 
-
-
 (defun zip-directory (directory-name)
   "Zip a directory name into the same name.zip"
   (interactive)
@@ -121,20 +114,6 @@ WARNING: this is a simple implementation. The chance of generating the same UUID
 		  (delete-file zip-file-name)
 		()) 
 	 (async-shell-command (concat "zip -r " zip-file-name " " directory-name))))
-
-(defun javascript-doc-skeleton()
-  "Insert a sekeleton javascript docstring"
-  (interactive)
-  (insert-string "/**\n")
-  (insert-string " * ")
-  (beginning-of-line)
-  (indent-relative)
-  (end-of-line)
-  (insert-string " \n*/")
-  (beginning-of-line)
-  (indent-relative)  
-  (previous-line)
-  (end-of-line))
 
 (defun msbuild-solution(solution-path)
   (interactive)
@@ -148,3 +127,89 @@ WARNING: this is a simple implementation. The chance of generating the same UUID
          (command-line (concat msbuild-path "/MSBuild.exe " solution-path " /p:DeployOnBuild=true /p:PublishProfile=" publish-profile)))
     (message command-line)
     (async-shell-command command-line)))
+
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+(require 'mu4e)
+(setq mu4e-get-mail-command "mbsync -a"
+      mu4e-update-interval 300)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(compilation-message-face 'default)
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes '(solarized-gruvbox-dark))
+ '(custom-safe-themes
+   '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
+ '(highlight-symbol-colors
+   '("#3b6b40f432d6" "#07b9463c4d36" "#47a3341e358a" "#1d873c3f56d5" "#2d86441c3361" "#43b7362d3199" "#061d417f59d7"))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   '(("#073642" . 0)
+     ("#5b7300" . 20)
+     ("#007d76" . 30)
+     ("#0061a8" . 50)
+     ("#866300" . 60)
+     ("#992700" . 70)
+     ("#a00559" . 85)
+     ("#073642" . 100)))
+ '(hl-bg-colors
+   '("#866300" "#992700" "#a7020a" "#a00559" "#243e9b" "#0061a8" "#007d76" "#5b7300"))
+ '(hl-fg-colors
+   '("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36"))
+ '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
+ '(lsp-ui-doc-border "#93a1a1")
+ '(nrepl-message-colors
+   '("#dc322f" "#cb4b16" "#b58900" "#5b7300" "#b3c34d" "#0061a8" "#2aa198" "#d33682" "#6c71c4"))
+ '(package-selected-packages
+   '(emms linum-relative racket-mode solarized-theme slime auto-complete))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#dc322f")
+     (40 . "#cb4366eb20b4")
+     (60 . "#c1167942154f")
+     (80 . "#b58900")
+     (100 . "#a6ae8f7c0000")
+     (120 . "#9ed892380000")
+     (140 . "#96be94cf0000")
+     (160 . "#8e5397440000")
+     (180 . "#859900")
+     (200 . "#77679bfc4635")
+     (220 . "#6d449d465bfd")
+     (240 . "#5fc09ea47092")
+     (260 . "#4c68a01784aa")
+     (280 . "#2aa198")
+     (300 . "#303498e7affc")
+     (320 . "#2fa1947cbb9b")
+     (340 . "#2c879008c736")
+     (360 . "#268bd2")))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   '(unspecified "#002b36" "#073642" "#a7020a" "#dc322f" "#5b7300" "#859900" "#866300" "#b58900" "#0061a8" "#268bd2" "#a00559" "#d33682" "#007d76" "#2aa198" "#839496" "#657b83"))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
